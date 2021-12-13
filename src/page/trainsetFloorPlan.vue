c275b4536076ca5ccfe6a7e6880793c11171bb02
chenshuaiyu <1961569938@qq.com>
2021-12-13 15:08:37 +0800

    调整f070列车类型判断逻辑

--- src/page/trainsetFloorPlan.vue
+++ src/page/trainsetFloorPlan.vue
@@ -2,7 +2,7 @@
   <div>
     <el-container>
       <el-header class="bg-color">
-        <Header ></Header>
+        <Header></Header>
       </el-header>
       <div class="bg-color">
         <multimedia-logo class="container" pagename="trainsetFloorPlan"></multimedia-logo>
@@ -24,47 +24,52 @@ import MultimediaLogo from "@/components/MultimediaLogo.vue";
 
 export default {
   components: { Header, MultimediaLogo },
-  data: function() {
+  data: function () {
     return {
       imagePath: ""
     };
   },
-  mounted: function() {
+  mounted: function () {
     let that = this;
-//that.$store.state.media_server + "/trainpic"
-   //axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    //that.$store.state.media_server + "/trainpic"
+    //axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     axios
       .get(that.$store.state.media_server + "/trainpic", {
-          params:{
-             lang: i18n.locale
-          },
-        })
-      .then(function(response) {
-        console.log(response);
+        params: {
+          lang: i18n.locale
+        },
+      })
+      .then(function (response) {
+        //console.log(response);
         console.log(that.getTrainType);
-        response["data"]["data"].forEach((element,index) => {
-          //response["data"]["data"][index]['type'] == that.getTrainType
-          if (response["data"]["data"][index]['type'] == '0x10') {
-              that.imagePath = that.$store.state.media_server + response["data"]["data"][index]['filepath'];
-              return;
+        response["data"]["data"].forEach((element, index) => {
+          if (response["data"]["data"][index]['type'] == that.getTrainType) {
+            that.imagePath = that.$store.state.media_server + response["data"]["data"][index]['filepath'];
+            return;
           }
         });
-        
       })
-      .catch(function(error) {
-        console.log(error);
+      .catch(function () {
+        //console.log(error);
         that.$message({
-            message: i18n.tc("message.networkError"),
-            type: "info",
-            center: true,
-            // iconClass: "",
-            duration: 1800,
-          });
+          message: i18n.tc("message.networkError"),
+          type: "info",
+          center: true,
+          // iconClass: "",
+          duration: 1800,
+        });
       });
   },
   computed: {
     getTrainType() {
-      return this.$store.state.trainInformation.trainType;
+      //070匹配发的列车类型，re02匹配0x10
+      if (this.$store.state.media_server.indexOf('172.19.6.20') != -1) {
+        return this.$store.state.trainInformation.trainType;
+      } else if (this.$store.state.media_server.indexOf('172.19.3.20') != -1) {
+        return '0x10';
+      } else {
+        return '';
+      }
     },
   },
   beforeRouteEnter(to, from, next) {
